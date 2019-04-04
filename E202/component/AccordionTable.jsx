d012737462e5b95bc8147ef4267f210f34d4fd73
Dulanjali-Liyanage <44173386+Dulanjali-Liyanage@users.noreply.github.com>
2019-04-04 14:06:04 +0530

    Add files via upload

---
+++ E202/component/AccordionTable.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemPanel,
+  AccordionItemButton
+} from "react-accessible-accordion";
+
+class AccordionTable extends Component {
+  state = {};
+  render() {
+    return (
+      <Accordion>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              What harsh truths do you prefer to ignore?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+              occaecat ut occaecat consequat est minim minim esse tempor laborum
+              consequat esse adipisicing eu reprehenderit enim.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              Is free will real or just an illusion?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+              nostrud velit in irure cillum tempor laboris sed adipisicing eu
+              esse duis nulla non.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+      </Accordion>
+    );
+  }
+}
+
+export default AccordionTable;
